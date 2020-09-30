7612a7dddcf48e3516652633df54b1a320332ce4
dongwooh <sapot1998@gmail.com>
2020-09-30 21:12:16 +0900

    Card Component and TTS in DeliveryCall

--- src/Screens/Delievery/DelieveryCall.js
+++ src/Screens/Delievery/DelieveryCall.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {Card} from '@paraboly/react-native-card';
 
 import Tts from 'react-native-tts';
 
@@ -15,10 +16,32 @@ const DelieveryCall = () => {
   };
 
   return (
-    <View>
-      <Text>대윤파닭 </Text>
+    <View style={styles.container}>
+      <Card
+        title="대윤파닭"
+        onPress={() => {}}
+        borderRadius={20}
+        bottomRightText="30 km"
+        iconBackgroundColor="#32D8BA"
+        textContainerNumberOfLines={null}
+        content="대윤파닭"
+        bottomRightStyle={{
+          fontSize: 16,
+          fontWeight: 'bold',
+          color: '#505e80',
+        }}
+      />
     </View>
   );
 };
 
 export default DelieveryCall;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+});
