4244fdc7c472d8894d8fcd44a123f53f69add1bd
dongwooh <sapot1998@gmail.com>
2020-09-30 13:47:57 +0900

    homepage Style

--- App.js
+++ App.js
@@ -1,59 +1,156 @@
 import 'react-native-gesture-handler';
 import React, {useEffect} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
 import SplashScreen from 'react-native-splash-screen';
 
-import PendingScreen from './src/Screens/Pending';
-import OnGoingScreen from './src/Screens/OnGoing';
-import CompleteScreen from './src/Screens/Complete';
-import Seperator from './src/Bluetooth/seperator';
-import Bluetooth from './src/Bluetooth/';
+import Delievery from './src/Screens/Delievery';
 
 const Tab = createMaterialTopTabNavigator();
 const Stack = createStackNavigator();
 
 function HomeScreen({navigation}) {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Button
-        title="블루투스"
-        onPress={() => navigation.navigate('블루투스')}
-      />
-      <Seperator />
-      <Button title="배달" onPress={() => navigation.navigate('배달현황')} />
+    <View style={styles.wrapper}>
+      <Text style={styles.title}>안녕하세요,</Text>
+      <Text style={styles.nameFirst}>
+        <Text style={styles.nameFirst}>오늘의</Text>
+        <Text style={styles.nameSecond}> 배달</Text>
+        <Text style={styles.nameFirst}>을</Text>
+      </Text>
+      <Text style={styles.nameThird}> 시작해봐요</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('배달')}>
+        <Text style={styles.buttonName}>시작</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const root = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="대기" component={PendingScreen} />
-      <Tab.Screen name="진행" component={OnGoingScreen} />
-      <Tab.Screen name="완료" component={CompleteScreen} />
-    </Tab.Navigator>
-  );
-};
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
-
   return (
-    <>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="블루투스" component={Bluetooth} />
-          <Stack.Screen name="배달현황" component={root} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="배달"
+          component={Delievery}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  wrapperLeft: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    borderColor: 'grey',
+    borderWidth: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  wrapperName: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    marginLeft: 15,
+  },
+  title: {
+    position: 'absolute',
+    width: 319,
+    height: 70,
+    left: 40,
+    top: 147,
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 64,
+    lineHeight: 76,
+
+    color: '#000000',
+  },
+  nameFirst: {
+    position: 'absolute',
+    width: 310,
+    height: 70,
+    left: 60,
+    top: 380,
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 50,
+    lineHeight: 59,
+
+    color: '#000000',
+  },
+  nameSecond: {
+    position: 'absolute',
+    width: 310,
+    height: 70,
+    left: 60,
+    top: 380,
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 50,
+    lineHeight: 59,
+
+    color: '#32D8BA',
+  },
+  nameThird: {
+    position: 'absolute',
+    width: 338,
+    height: 100,
+    left: 110,
+    top: 456,
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 50,
+    lineHeight: 59,
+
+    color: '#000000',
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    width: 193,
+    height: 60,
+    left: 87,
+    top: 625,
+    borderRadius: 90,
+    backgroundColor: '#32D8BA',
+  },
+  buttonName: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 30,
+    lineHeight: 35,
+
+    color: '#000',
+  },
+});
